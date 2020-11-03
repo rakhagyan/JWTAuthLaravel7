f7f485792a8feab9ce0219d9e3fcf792f54b0a3a
Rakha Putra Pratama <63826304+rakhagyan@users.noreply.github.com>
2020-11-03 10:33:00 +0700

    Upload Folder Config + Database

---
+++ database/migrations/2020_11_02_103427_create_siswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->date('tanggal_lahir');
+            $table->enum('gender', ['L', 'P']);
+            $table->string('alamat');
+            $table->integer('nik')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
